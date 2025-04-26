6a0ca504549831de492eb9a789b05843487ea2d0
Frivack <78854503+Frivack@users.noreply.github.com>
2025-04-27 02:52:51 +0900

    응답 메세지 추가

--- main.py
+++ main.py
@@ -13,8 +13,14 @@ db_config = {
 # 허용할 테이블 목록
 allowed_tables = ['reviews', 'users']
 
+# 기본 페이지
+@app.route('/', methods=['GET'])
+def index():
+    return 'Server is running!', 200
+
+# 테이블 데이터 조회
 @app.route('/get-data/<table_name>', methods=['GET'])
-def get_data(table_name):
+def get_data_route(table_name):
     if table_name not in allowed_tables:
         return jsonify({'error': 'Invalid table name'}), 400
 
